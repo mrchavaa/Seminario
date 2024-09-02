ae09d203b5eb201da559f4045dc87065c57dc371
ANGEL ALEXANDER RAMÍREZ NAVARRO <angel.ramirez5119@alumnos.udg.mx>
2024-09-02 15:06:32 -0600

    Archivo codigo.cpp modificado

--- codigo.cpp
+++ codigo.cpp
@@ -6,5 +6,9 @@ int main()
 {
     setlocale(LC_ALL, "spanish");
     cout << "Hola profesor Rubén :)" << endl;
+
+    /*ACABO DE AGREGAR ESTAS LÍNEAS PARA QUE
+    SE VEAN LOS CAMBIOS EN GITHUB*/
+
     return 0;
 }
